a5744b04bf5d4a2e621f313dcf4e0a65b00383c2
adam.gladstone@insync.cloud <adam.gladstone@insync.cloud>
2016-11-30 06:15:25 +0000

    added user  validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-    
+  validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, length: {minimum: 3, maximum: 105}, 
+                    format: { with: VALID_EMAIL_REGEX}
+
 end
